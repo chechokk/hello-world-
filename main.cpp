7b8b99384e455c120d649f5dbc3c8df467c8687d
Daniil <daniil.cheberiak@gmail.com>
2026-09-13 10:10:19 +0300

    My new name

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     cout << "Hello from feature" << endl;
     cout<<"\n";
-    cout<<"My name is Daniil"<<endl;
+    cout<<"My name is not Daniil"<<endl;
     return 0;
 }
